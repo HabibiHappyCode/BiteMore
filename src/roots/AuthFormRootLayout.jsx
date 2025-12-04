5567f75f592c27a7a8a6f54d77a57a390fdbb780
Iyiola Abdulmalik <abdulmalikiyiola@gmail.com>
2025-12-04 09:12:43 -0800

    scroll to top on routing

--- src/roots/AuthFormRootLayout.jsx
+++ src/roots/AuthFormRootLayout.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import { Outlet } from 'react-router-dom'
 import NormalNavBar from '../navigations/NormalNavbar'
 import Footer from '../footer/Footer'
+import ScrollToTop from '../Ui/ScrollToTop'
 
 function AuthFormRootLayout() {
     return (
         <div>
+            <ScrollToTop />
             <NormalNavBar />
             <Outlet />
             <Footer />
--- src/roots/NormalRootLayouts.jsx
+++ src/roots/NormalRootLayouts.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import { Outlet } from 'react-router-dom'
 import NormalNavbar from '../navigations/NormalNavbar'
 import Footer from '../footer/Footer'
+import ScrollToTop from '../Ui/ScrollToTop'
 
 function NormalRootLayouts() {
     return (
         <>
+            <ScrollToTop />
             <NormalNavbar />
             <Outlet />
             <Footer />
